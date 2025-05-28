044544a4dbf1c8287c03b94283b26ba84453fd9a
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-05-28 23:52:44 +0530

    Dashboard

--- src/components/Boxes.jsx
+++ src/components/Boxes.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Boxes = () => {
   return (
-    <div className='h-[108px] w-[326px] bg-amber-100 rounded-[12px]'>
+    <div className='h-[108px] w-[326px] bg-[#111111] rounded-[12px] flex justify-center items-center'>
+        <div className='h-[56px] w-[276px] bg-white flex '>
+            {/* left side Image */}
+            <div className='w-[20%] h-full bg-amber-100'>
+                
+            </div>
 
+            {/* rightside text div */}
+            <div className='w-[80%] h-full bg-amber-200'>
+
+            </div>
+        </div>
     </div>
   )
 }
