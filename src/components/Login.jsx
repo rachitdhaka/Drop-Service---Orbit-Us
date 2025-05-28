1dd19fa72cc81844b9bff2edee484815fd96e477
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-05-28 14:10:19 +0530

    Login & Sign up page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className='h-screen bg-black flex flex-col justify-center items-center'>
-        
-        <section className='bg-black  rounded-2xl w-xl p-5  flex flex-col justify-center items-center'>
-            <h1 className=" text-white text-3xl font-semibold ">
-              Welcome Back
-            </h1>
+    <div className="h-screen bg-black flex flex-col justify-center items-center">
+      <section className="bg-black  rounded-2xl w-xl p-5  flex flex-col justify-center items-center">
+        <h1 className=" text-white text-3xl font-semibold ">Welcome Back</h1>
 
-            <button className='border border-[#00FF94] w-xs  rounded-xl text-[#00FF94]  p-2 my-4 bg-[#111111] cursor-pointer text-xs'>
-              Connect Wallet
-            </button>
+        <button className="border border-[#00FF94] w-xs  rounded-xl text-[#00FF94]  p-2 my-4 bg-[#111111] cursor-pointer text-xs">
+          Connect Wallet
+        </button>
 
-            
-            <p className='text-[10px] text-[#9CA3AF]'>
-              or conitnue with
-            </p>
+        <p className="text-[10px] text-[#9CA3AF]">or conitnue with</p>
 
-            
-            <input type="email" placeholder='Email' className='placeholder:text-xs text-sm border border-[#1F2937] w-xs  rounded-xl text-[#9CA3AF]  p-2 my-2 bg-[#111111] focus:outline-none'/>
-            <input type="password" placeholder='Password' className='placeholder:text-xs text-sm  border border-[#1F2937] w-xs  rounded-xl text-[#9CA3AF]  p-2 my-2 bg-[#111111] focus:outline-none'/>
+        <input
+          type="email"
+          placeholder="Email"
+          className="placeholder:text-xs text-sm border border-[#1F2937] w-xs  rounded-xl text-[#9CA3AF]  p-2 my-2 bg-[#111111] focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="placeholder:text-xs text-sm  border border-[#1F2937] w-xs  rounded-xl text-[#9CA3AF]  p-2 my-2 bg-[#111111] focus:outline-none"
+        />
 
-            <button className='text-white bg-[linear-gradient(90deg,_#B026FF_0%,_#00FF94_100%)] h-10 w-24 rounded-xl mt-5 cursor-pointer text-xs'>
-              Sign Up
-            </button>
+        <button className="text-white bg-[linear-gradient(90deg,_#B026FF_0%,_#00FF94_100%)] h-10 w-24 rounded-xl mt-5 cursor-pointer text-xs">
+          Sign Up
+        </button>
 
-            <p className='text-white text-xs mt-3'>
-              Dont Have an account ? {" "}
-              <span className='text-[#00FF94]'>
-                <a href="#">
-                  Create One
-                </a>
-              </span>
-            </p>
-            
-
-            
-        </section>
+        <NavLink to="/signup" className="text-white text-xs mt-3">
+         Dont have an Account ?{" "}
+          <span className="text-[#00FF94]">
+            Create One
+          </span>
+        </NavLink>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
