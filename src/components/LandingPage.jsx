3d8ed3af714af783b87195963acd8fbfa52fb8e8
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-05-28 22:02:39 +0530

    Navbar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className='h-screen bg-black text-white flex justify-center items-center'>Landing Page - Coming Soon</div>
-  )
-}
+    <div className="bg-black h-screen flex flex-col justify-center items-center gap-3">
 
-export default LandingPage
+        <p className="text-white">This is the Landing Page</p>
+      <NavLink to="/dashboard" className="text-white text-xs mt-3"> 
+        <span className="text-white bg-neutral-700 p-3 rounded-2xl  ">Dashboard</span>
+      </NavLink>
+    </div>
+  );
+};
+
+export default LandingPage;
